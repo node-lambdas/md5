b4f0c9340864d366bc5065f5a8b95cfa484e0aa0
Darlan Alves <me@darlanalv.es>
2020-08-23 00:37:16 +0200

    feat: update to v2

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import { lambda, Format } from '@node-lambdas/core';
 import { createHash } from 'crypto';
 
 const configuration = {
-  version: 1,
-  input: Format.Buffer,
+  version: 2,
+  actions: {
+    default: {
+      input: Format.Buffer,
+      handler: (input, output) => output.send(createHash('md5').update(input.body).digest('hex')),
+    },
+  },
 };
 
-lambda(configuration, (input, output) => output.send(createHash('md5').update(input.body).digest('hex')));
+lambda(configuration);
